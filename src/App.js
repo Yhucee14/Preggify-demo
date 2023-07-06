a5c7f47fff6dfa4a652574d710a3402a0e7b6125
Yhucee14 <uchechukwu7814@gmail.com>
2023-07-06 02:45:48 -0700

    implemented server side rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
+import React from 'react';
 import {TopNavbar, MainNavbar, Body} from './components'
 import styles from './styles'
 
 function App() {
   return (
     <div className=" w-full overflow-hidden ">
-      <div>
-        <div> <TopNavbar /> </div>
-        <div> <MainNavbar /> </div>
-      </div>
-
-      <div >
-        <div className={`${styles.boxWidth}`}>
-          <Body />
-       </div>
-      </div>
-    
+    <div>
+      <div> <TopNavbar /> </div>
+      <div> <MainNavbar /> </div>
     </div>
+
+    <div >
+      <div className={`${styles.boxWidth}`}>
+        <Body />
+     </div>
+    </div>
+  
+  </div>
   );
-}
+};
 
 export default App;
